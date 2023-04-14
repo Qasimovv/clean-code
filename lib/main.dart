015bb6d316185324f4b0bce4bd53584e2c3332fe
Elnur Qasimov <70575033+Qasimovv@users.noreply.github.com>
2023-04-14 21:57:24 +0400

    add color

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(),
+        body: Container(
+          color: Colors.yellow,
+        ),
       ),
     );
   }
